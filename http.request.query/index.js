4bd1aec3f92b2fec922188ad2d6ab984e0c6f418
Jong <Stefan.de.Jong.wia@ing.com>
2019-03-09 10:32:12 +0100

    async/await

--- http.request.query/index.js
+++ http.request.query/index.js
@@ -4,14 +4,12 @@ module.exports = (NODE) => {
   const reqsIn = NODE.getInputByName('requests');
 
   const valuesOut = NODE.getOutputByName('values');
-  valuesOut.on('trigger', (conn, state, callback) => {
-    reqsIn.getValues(state)
-    .then((requests) => {
-      const queries = requests
-      .filter(req => req && req.query && req.query[NODE.data.paramName])
-      .map(req => req.query[NODE.data.paramName]);
+  valuesOut.on('trigger', async (conn, state) => {
+    const requests = await reqsIn.getValues(state)
+    const queries = requests
+    .filter(req => req && req.query && req.query[NODE.data.paramName])
+    .map(req => req.query[NODE.data.paramName]);
 
-      callback(queries);
-    });
+    return queries;
   });
 };
